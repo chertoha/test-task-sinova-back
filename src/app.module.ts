aa6df8e918c9c89aeac4f1083fe5a59957688d9b
Anton <a.chertok@akatech.com.ua>
2024-10-24 14:06:51 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { PostsModule } from "./posts/posts.module";
 import { ConfigModule } from "@nestjs/config";
 import { FilesModule } from "./files/files.module";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
-import { PUBLIC_FOLDER_NAME } from "./utils/constants";
 
 @Module({
  imports: [
